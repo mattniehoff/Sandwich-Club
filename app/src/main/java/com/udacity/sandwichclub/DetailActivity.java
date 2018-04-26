2aad6feebab17299080ad3615b2fa69a6780ae28
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-04-25 21:23:11 -0500

    Changes to LinearLayout in a ScrollView and populates the UI with text data.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView originTextView;
+    private TextView descriptionTextView;
+    private TextView ingredientsTextView;
+    private TextView akaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView pictureImageView = findViewById(R.id.image_iv);
+
+        originTextView = findViewById(R.id.origin_tv);
+        descriptionTextView = findViewById(R.id.description_tv);
+        ingredientsTextView = findViewById(R.id.ingredients_tv);
+        akaTextView = findViewById(R.id.also_known_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +58,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(pictureImageView);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +71,24 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        originTextView.setText(sandwich.getPlaceOfOrigin());
+        descriptionTextView.setText(sandwich.getDescription());
+        ingredientsTextView.setText(stringListToString(sandwich.getIngredients()));
+        akaTextView.setText(stringListToString(sandwich.getAlsoKnownAs()));
+    }
 
+    private String stringListToString(List<String> listString) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < listString.size(); i++) {
+            if (i == 0) {
+                sb.append(listString.get(i));
+            } else {
+                sb.append(", ");
+                sb.append(listString.get(i));
+            }
+        }
+
+        return sb.toString();
     }
 }
